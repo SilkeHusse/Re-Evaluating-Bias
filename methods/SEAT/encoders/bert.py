84b9091e84fdd797a78ec685c097f83d890b4bb6
Silke Husse <silke.husse@uni.kn>
2022-03-13 13:56:33 +0100

    commenting

--- methods/SEAT/encoders/bert.py
+++ methods/SEAT/encoders/bert.py
@@ -9,7 +9,7 @@ dirname = os.path.dirname(os.path.realpath(__file__))
 models_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(dirname))), 'models')
 
 def load_model():
-    """ Load BERT model (bbc) and corresponding tokenizer from local files  """
+    """ Load BERT model (bbc) and corresponding tokenizer from local files """
 
     model = BertModel.from_pretrained(models_dir + '/bert/base-cased/')
     tokenizer = BertTokenizer.from_pretrained(models_dir + '/bert/base-cased/')
